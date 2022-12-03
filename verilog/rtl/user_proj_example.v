66181209326f26796fcf48e2650166d517a79f19
Vision-VLSI Industry PVT. LTD <vvi.pvtltd@gmail.com>
2022-12-03 13:53:41 +0530

    Update user_proj_example.v

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -68,98 +68,129 @@ module user_proj_example #(
     // IRQ
     output [2:0] irq
 );
-    wire clk;
-    wire rst;
-
     wire [`MPRJ_IO_PADS-1:0] io_in;
     wire [`MPRJ_IO_PADS-1:0] io_out;
     wire [`MPRJ_IO_PADS-1:0] io_oeb;
+    
 
-    wire [31:0] rdata; 
-    wire [31:0] wdata;
-    wire [BITS-1:0] count;
-
-    wire valid;
-    wire [3:0] wstrb;
-    wire [31:0] la_write;
-
-    // WB MI A
-    assign valid = wbs_cyc_i && wbs_stb_i; 
-    assign wstrb = wbs_sel_i & {4{wbs_we_i}};
-    assign wbs_dat_o = rdata;
-    assign wdata = wbs_dat_i;
-
-    // IO
-    assign io_out = count;
-    assign io_oeb = {(`MPRJ_IO_PADS-1){rst}};
-
-    // IRQ
-    assign irq = 3'b000;	// Unused
-
-    // LA
-    assign la_data_out = {{(127-BITS){1'b0}}, count};
-    // Assuming LA probes [63:32] are for controlling the count register  
-    assign la_write = ~la_oenb[63:32] & ~{BITS{valid}};
-    // Assuming LA probes [65:64] are for controlling the count clk & reset  
-    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
-    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
-
-    counter #(
-        .BITS(BITS)
-    ) counter(
-        .clk(clk),
-        .reset(rst),
-        .ready(wbs_ack_o),
-        .valid(valid),
-        .rdata(rdata),
-        .wdata(wbs_dat_i),
-        .wstrb(wstrb),
-        .la_write(la_write),
-        .la_input(la_data_in[63:32]),
-        .count(count)
-    );
+    KSA16 KSA16ins(.sum(io_out[15:0]), .cout(io_out[16]), .a(io_in[15:0]), .b(io_in[31:16]));
+    assign io_oeb=0;
 
 endmodule
 
-module counter #(
-    parameter BITS = 32
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output ready,
-    output [BITS-1:0] rdata,
-    output [BITS-1:0] count
-);
-    reg ready;
-    reg [BITS-1:0] count;
-    reg [BITS-1:0] rdata;
+module BigCircle(output G, P, input Gi, Pi, GiPrev, PiPrev);
+  
+  wire e;
+  and  (e, Pi, GiPrev);
+  or  (G, e, Gi);
+  and  (P, Pi, PiPrev);
+  
+endmodule
 
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 0;
-            ready <= 0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-                if (wstrb[2]) count[23:16] <= wdata[23:16];
-                if (wstrb[3]) count[31:24] <= wdata[31:24];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
-            end
-        end
+module SmallCircle(output Ci, input Gi);
+  
+ // buf  (Ci, Gi);
+    assign Ci=Gi;
+  
+endmodule
+
+module Square(output G, P, input Ai, Bi);
+  
+  and  (G, Ai, Bi);
+  xor  (P, Ai, Bi);
+  
+endmodule
+
+module Triangle(output Si, input Pi, CiPrev);
+  
+  xor  (Si, Pi, CiPrev);
+  
+endmodule
+
+
+module KSA16(output [15:0] sum, output cout, input [15:0] a, b);
+  
+  wire cin = 1'b0;
+  wire [15:0] c;
+  wire [15:0] g, p;
+    
+    
+    
+    genvar i,j;
+    generate for(i=0; i<16; i=i+1) begin : test1
+  //Square sq[15:0](g, p, a, b);
+        Square sq(g[i], p[i], a[i], b[i]);
     end
+    endgenerate
 
+  // first line of circles
+  wire [15:1] g2, p2;
+  SmallCircle sc0_0(c[0], g[0]);
+ 
+    generate for(j=1; j<16; j=j+1) begin : test2
+  //BigCircle bc0[15:1](g2[15:1], p2[15:1], g[15:1], p[15:1], g[14:0], p[14:0]);
+        BigCircle bc0(g2[j], p2[j], g[j], p[j], g[j-1], p[j-1]);
+    end
+    endgenerate
+
+  // second line of circle
+  wire [15:3] g3, p3;
+    genvar k;
+    generate for(k=1; k<3; k=k+1) begin : test2
+  //SmallCircle sc1[2:1](c[2:1], g2[2:1]);
+        SmallCircle sc1(c[k], g2[k]);
+    end
+    endgenerate
+
+    genvar i1;
+    generate for(i1=3;i1<16;i1=i1+1) begin: test3
+  //BigCircle bc1[15:3](g3[15:3], p3[15:3], g2[15:3], p2[15:3], g2[13:1], p2[13:1]);
+        BigCircle bc1(g3[i1], p3[i1], g2[i1], p2[i1], g2[i1-2], p2[i1-2]);
+    end
+    endgenerate
+  // third line of circle
+  wire [15:7] g4, p4;
+  
+    genvar j1;
+    generate for(j1=3; j1<7; j1=j1+1) begin:test4
+  //SmallCircle sc2[6:3](c[6:3], g3[6:3]);
+        SmallCircle sc2(c[j1], g3[j1]);
+    end
+    endgenerate
+    genvar k1;
+    generate for(k1=7; k1<16; k1=k1+1) begin:test5
+  //BigCircle bc2[15:7](g4[15:7], p4[15:7], g3[15:7], p3[15:7], g3[11:3], p3[11:3]);
+        BigCircle bc2(g4[k1], p4[k1], g3[k1], p3[k1], g3[k1-4], p3[k1-4]);
+    end
+    endgenerate
+
+  // fourth line of circle
+  wire [15:15] g5, p5;
+  
+    genvar m1,m2;
+    generate for(m1=7; m1<15; m1=m1+1) begin:test6
+
+  //SmallCircle sc3[14:7](c[14:7], g4[14:7]);
+        SmallCircle sc3(c[m1], g4[m1]);
+    end
+        endgenerate
+
+  BigCircle bc3_15(g5[15], p5[15], g4[15], p4[15], g4[7], p4[7]);  
+
+  // fifth line of circle
+  SmallCircle sc4_15(c[15], g5[15]);
+
+  // last line - triangles
+  Triangle tr0(sum[0], p[0], cin);
+  //Triangle tr[15:1](sum[15:1], p[15:1], c[14:0]);
+    generate for(m2=1; m2<16; m2=m2+1) begin:test7
+  //Triangle tr[15:1](sum[15:1], p[15:1], c[14:0]);
+        Triangle tr1(sum[m2], p[m2], c[m2-1]);
+    end
+    endgenerate
+
+  // generate cout
+ // buf (cout, c[15]);
+    assign cout=c[15];
 endmodule
 `default_nettype wire
