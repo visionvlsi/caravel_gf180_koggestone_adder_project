168d95d6f71bda51167e5fd2255aa49cd0adbb49
Vision-VLSI Industry PVT. LTD <vvi.pvtltd@gmail.com>
2022-12-24 20:07:38 +0530

    Update KSA16.v

--- verilog/rtl/KSA16.v
+++ verilog/rtl/KSA16.v
@@ -1,12 +1,5 @@
 `default_nettype none
-module BigCircle(output G, P, input Gi, Pi, GiPrev, PiPrev);
-  
-  wire e;
-  and  (e, Pi, GiPrev);
-  or  (G, e, Gi);
-  and  (P, Pi, PiPrev);
-  
-endmodule
+
 
 module SmallCircle(output Ci, input Gi);
   
